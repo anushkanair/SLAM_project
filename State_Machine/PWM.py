c2ec497558e81c2944a53126a188c61d536d206a
anushkanair <bringthematter@gmail.com>
2017-12-28 13:12:56 -0800

    Remove old files

--- State_Machine/PWM.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-def run_motors_redis(value, value2):
-    r.set("motor_left_pwm", value)
-    r.set("motor_right_pwm", value2)
-
-//run_motors_redis(10, 10)
--- State_Machine/redis_test.py
+++
@@ -1,7 +0,0 @@
-import redis
-
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-r.set("motor_left_pwm", "10")
-PWM = r.get("motor_left_pwm")
-print int(PWM)
