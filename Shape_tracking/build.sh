0caee5ebf3aff0728dc5d7d9c073a1edccfe017f
anushkanair <bringthematter@gmail.com>
2017-12-28 16:23:40 -0800

    Added a simple build script

---
+++ Shape_tracking/build.sh
@@ -0,0 +1,2 @@
+g++ -O3 -std=c++11 -lhiredis -lopencv_core -lopencv_calib3d -lopencv_videoio -lopencv_imgproc -lopencv_highgui ../shapeTracking.cpp
+#g++ -O3 -lopencv_imgcodecs -lopencv_core -lopencv_videoio ../save_img.cpp -o save_img
